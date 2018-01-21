8299e6c6571fe2f4595fea43a4107bd61de32f14
AliBahramiEhsan <work@bahramiehsan.com>
2018-01-21 22:45:18 +0330

    adding neo test

--- front/color-pallete/src/app/app-routing.module.ts
+++ front/color-pallete/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserInfoGetComponent } from './components/user-info-get/user-info-get.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NeoPageComponent } from './components/neo-page/neo-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent, pathMatch: 'full' },
   { path: 'userInfo', component: UserInfoGetComponent },
+  { path: 'neoTest', component: NeoPageComponent },
 ];
 
 @NgModule({
--- front/color-pallete/src/app/app.module.ts
+++ front/color-pallete/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { UserInfoGetComponent } from './components/user-info-get/user-info-get.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NeoPageComponent } from './components/neo-page/neo-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserInfoGetComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NeoPageComponent
   ],
   imports: [
     BrowserModule,
